5c4b500bc9ea438bf6c4d4994765996c2f5a4bbd
Arkadiy <pv487732@gmail.com>
2024-11-03 00:33:35 +0300

    Zenject + Npc idling

--- Assets/Scripts/Configs/Enemy/Movement/EnemyMovementCfg.cs
+++ Assets/Scripts/Configs/Enemy/Movement/EnemyMovementCfg.cs
@@ -6,6 +6,7 @@ namespace Configs.Enemy.Movement
     public class EnemyMovementCfg : ScriptableObject
     {
         [field: SerializeField] public float Speed;
+        [field: SerializeField] public int IdlingTimer;
         
     }
 }
--- Assets/Scripts/Enemy/AI/EnemyMovement.cs
+++ Assets/Scripts/Enemy/AI/EnemyMovement.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using Configs.Enemy.Movement;
 using UnityEngine;
 using UnityEngine.AI;
@@ -8,7 +9,9 @@ public class EnemyMovement : MonoBehaviour
    [SerializeField] private NavMeshAgent _agent;
    [SerializeField] private Transform[] _points;
 
-   [SerializeField] private int _currentPointIndex = 0;
+   private Coroutine _cor;
+   private int _currentPointIndex = 0;
+   private bool _isIdling;
    
    private void Awake()
    {
@@ -18,12 +21,29 @@ public class EnemyMovement : MonoBehaviour
 
    private void Update()
    {
-      if(_agent.remainingDistance >= 1f)
+      if(_agent.remainingDistance >= 1f || _isIdling)
          return;
+      _cor = StartCoroutine(IdlingTimer());
       if (_currentPointIndex >= _points.Length - 1)
          _currentPointIndex = 0;
       else
          _currentPointIndex++;
       _agent.SetDestination(_points[_currentPointIndex].position);
+   }
+
+   private IEnumerator IdlingTimer()
+   {
+      _isIdling = true;
+      yield return new WaitForSeconds(_movementCfg.IdlingTimer);
+      _isIdling = false;
+      _cor = null;
+   }
+
+   public void SetDestinationToSound(Transform soundTransform)
+   {
+      if(_cor is not null)
+         StopCoroutine(_cor);
+      _isIdling = false;
+      _agent.SetDestination(soundTransform.position);
    }
 }
